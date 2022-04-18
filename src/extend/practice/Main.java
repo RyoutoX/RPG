274aadf7814a0639fbebc5edb3138509cb30bcc2
ryoto <102937650+RyoutoX@users.noreply.github.com>
2022-04-18 10:04:58 +0900

    課題提出

--- src/extend/practice/Main.java
+++ src/extend/practice/Main.java
@@ -38,23 +38,24 @@ public class Main {
 // 戦闘開始
 		while (true) {
 			// 勇者チームの乱数インスタンス
-			int hnkougek = Rand.get(braveteam.size());
+			int hnkougek1 = Rand.get(braveteam.size());
 			// 勇者チームの乱数取得
-			Human hnrandom = braveteam.get(hnkougek);
+			Human hnrandom1 = braveteam.get(hnkougek1);
 
 			// モンスターチームの乱数インスタンス
-			int mskougeki = Rand.get(monsterteam.size());
+			int mskougeki1 = Rand.get(monsterteam.size());
 			// モンスターチームの乱数取得
-			Monster msrandom = monsterteam.get(mskougeki);
+			Monster msrandom1 = monsterteam.get(mskougeki1);
+
 			
 			// 勇者チームのターン
 			System.out.println("人間のターン！");
 			// 勇者チームが攻撃する。
-			hnrandom.attack(msrandom);
+			hnrandom1.attack(msrandom1);
 			// 敵のhpが０になったらリストの外に出す。
-			if (msrandom.hp <= 0) {
-				monsterteam.remove(msrandom);
-				System.out.println(msrandom.name + "は倒れた。");
+			if (msrandom1.hp <= 0) {
+				monsterteam.remove(msrandom1);
+				System.out.println(msrandom1.name + "は倒れた。");
 				// リストが空になったら勝ち
 				if (monsterteam.isEmpty() == true) {
 					System.out.println("勇者達は勝利した！");
@@ -64,12 +65,22 @@ public class Main {
 			// 人間のターン終了
 			// モンスターチームのターン
 			System.out.println("モンスターのターン！");
+			// 勇者チームの乱数インスタンス
+			int hnkougek2 = Rand.get(braveteam.size());
+			// 勇者チームの乱数取得
+			Human hnrandom3 = braveteam.get(hnkougek2);
+
+			// モンスターチームの乱数インスタンス
+			int mskougeki2 = Rand.get(monsterteam.size());
+			// モンスターチームの乱数取得
+			Monster msrandom3 = monsterteam.get(mskougeki2);
+
 			// モンスターが攻撃する。
-			msrandom.attack(hnrandom);
+			msrandom3.attack(hnrandom3);
 			// 敵のhpが０になったらリストの外に出す。
-			if (hnrandom.hp <= 0) {
-				braveteam.remove(hnrandom);
-				System.out.println(hnrandom.name + "は倒れた。");
+			if (hnrandom3.hp <= 0) {
+				braveteam.remove(hnrandom3);
+				System.out.println(hnrandom3.name + "は倒れた。");
 				// リストが空になったら勝ち
 				if (braveteam.isEmpty() == true) {
 					System.out.println("モンスター達は勝利した！");
